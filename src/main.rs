ae27b315928b3258f97b356dea66edb5611484cd
okt <web@evanfiddes.com>
2020-11-05 14:33:38 -0700

    init commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use rltk::{Rltk, GameState};
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx : &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rougelike World! Cyborg 01 init.");
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("borgrl - internal alpha 00")
+        .build()?;
+    let gs = State{ };
+    rltk::main_loop(context, gs)
+}
