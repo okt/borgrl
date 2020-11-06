ba64e1094213fb87d744856da15b53d77c4448d3
okt <web@evanfiddes.com>
2020-11-06 12:32:49 -0700

    Include Specs in the project

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use rltk::{Rltk, GameState};
+use rltk::{GameState, Rltk, VirtualKeyCode, RGB};
+use specs::prelude::*;
+use specs_derive::Component;
+use std::cmp::{max, min};
 
 struct State {}
 impl GameState for State {
-    fn tick(&mut self, ctx : &mut Rltk) {
+    fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
         ctx.print(1, 1, "Hello Rougelike World! Cyborg 01 init.");
     }
@@ -13,6 +16,6 @@ fn main() -> rltk::BError {
     let context = RltkBuilder::simple80x50()
         .with_title("borgrl - internal alpha 00")
         .build()?;
-    let gs = State{ };
+    let gs = State {};
     rltk::main_loop(context, gs)
 }
