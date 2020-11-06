e5750777fb20b834840caf53350eb4a3d20a9512
okt <web@evanfiddes.com>
2020-11-06 16:27:00 -0700

    making moves!

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ struct Renderable {
     bg: RGB,
 }
 
+#[derive(Component)]
+struct LeftMover {}
+
 struct State {
     ecs: World,
 }
@@ -23,13 +26,38 @@ struct State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        // ctx.print(1, 1, "Hello Rougelike World! Cyborg 01 init.");
+
+        self.run_systems();
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
+    }
+}
+
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 {
+                pos.x = 79;
+            }
+        }
+    }
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker {};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
     }
 }
 
@@ -41,7 +69,7 @@ fn main() -> rltk::BError {
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-
+    gs.ecs.register::<LeftMover>();
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -52,18 +80,16 @@ fn main() -> rltk::BError {
         })
         .build();
 
-    for i in 0..10 {
+    for i in 0..30 {
         gs.ecs
             .create_entity()
-            .with(Position {
-                x: i * 2,
-                y: i + 20,
-            })
+            .with(Position { x: i * 2, y: 20 })
             .with(Renderable {
                 glyph: rltk::to_cp437('φ'),
                 fg: RGB::named(rltk::RED),
                 bg: RGB::named(rltk::BLACK),
             })
+            .with(LeftMover {})
             .build();
     }
 
